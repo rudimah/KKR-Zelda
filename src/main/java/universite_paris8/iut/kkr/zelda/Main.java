61a0f7ce5b522176fe495983f7c7d6a66890c909
hrahman <hamidur.rahman@outlook.fr>
2024-05-16 11:23:14 +0200

    Image de Link quand il se déplace grace à un tileset

--- src/main/java/universite_paris8/iut/kkr/zelda/Main.java
+++ src/main/java/universite_paris8/iut/kkr/zelda/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Map1.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 800);
+        Scene scene = new Scene(fxmlLoader. load(), 800, 800);
         //scene.setOnKeyPressed();
         stage.setTitle("Hyrule - La fin des temps");
         stage.setScene(scene);
