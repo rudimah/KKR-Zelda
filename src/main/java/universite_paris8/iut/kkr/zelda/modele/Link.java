3ee0ed17ec536841d07691498aa82de98a901b78
hrahman <hamidur.rahman@outlook.fr>
2024-05-21 14:43:32 +0200

    Image de Link dans l'eau

--- src/main/java/universite_paris8/iut/kkr/zelda/modele/Link.java
+++ src/main/java/universite_paris8/iut/kkr/zelda/modele/Link.java
@@ -1,6 +1,6 @@
 package universite_paris8.iut.kkr.zelda.modele;
 
-import javafx.fxml.FXML;
+
 import javafx.geometry.Rectangle2D;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -8,8 +8,7 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.TilePane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
+
 
 public class Link extends ActeurEnMouvement{
 
@@ -20,7 +19,7 @@ public class Link extends ActeurEnMouvement{
     boolean pied_droite = true;
 
     public Link(Environnement env, Pane pj, TilePane tilePane) {
-        super(50, 50, 5, env, 40, 20);
+        super(50, 50, 2, env, 40, 10);
         this.panneauDeJeu = pj;
         this.tilePane = tilePane;
         this.imageView = new ImageView();
@@ -31,7 +30,7 @@ public class Link extends ActeurEnMouvement{
 
 
         imageView.setImage(image1);
-            imageView.setViewport(new Rectangle2D( 180,1250,120,160));
+        imageView.setViewport(new Rectangle2D( 20,13,120,160));
         imageView.setFitWidth(20);
         imageView.setFitHeight(30);
 
@@ -58,107 +57,96 @@ public class Link extends ActeurEnMouvement{
 
     public void seDeplacer(KeyCode key) {
 
-        int newX = getX(), newY = getY();
-
-        double x = 0, y = 0;
+        int nouveauX = getX(), nouveauY = getY();
+        int position_image_x = 20, position_image_y = 13, position_image_eau = 0;
 
         switch (key) {
             case Z:
-                newY = getY()-getVitesse();
-                y = 1050;
+                nouveauY = getY()-getVitesse();
+                position_image_y = 1060;
+                position_image_eau = 340;
                 if (pied_droite){
-                    x = 170;
+                    position_image_x = 170;
                     pied_droite= false;
                 }
                 else {
-                    x = 1470;
+                    position_image_x = 1470;
                     pied_droite = true;
                 }
 
                 break;
             case S:
-                newY = getY()+getVitesse();
-                y = 710;
+                nouveauY = getY()+getVitesse();
+                position_image_eau = 825;
+                position_image_y = 710;
                 if (pied_droite){
-                    x = 665;
+                    position_image_x = 665;
                     pied_droite= false;
                 }
                 else {
-                    x = 1470;
+                    position_image_x = 1470;
                     pied_droite = true;
                 }
 
 
                 break;
             case D:
-                newX = (getX()+getVitesse());
-                y = 1225;
+                nouveauX = (getX()+getVitesse());
+                position_image_eau = 340; //position x de l'image quand link va sur l'eau
+                position_image_y = 1245;
                 if (pied_droite){
-                    x = 180;
+                    position_image_x = 180;
                     pied_droite= false;
                 }
                 else {
-                    x = 1315;
+                    position_image_x = 1315;
                     pied_droite = true;
                 }
                 break;
             case Q:
-                newX = (getX()-getVitesse());
-                y = 895;
+                nouveauX = (getX()-getVitesse());
+                position_image_eau = 980;
+                position_image_y = 902;
                 if (pied_droite){
-                    x = 180;
+                    position_image_x = 180;
                     pied_droite= false;
                 }
                 else {
-                    x = 1315;
+                    position_image_x = 1320;
                     pied_droite = true;
                 }
                 break;
             default:
+                position_image_x = position_image_x;
+                position_image_y = position_image_y;
                 ;
         }
         System.out.println("Essaye de passer de " + getX() + ", " + getY() );
 
-        if (env.estPositionValide(newX, newY)) {
+        if (getEnv().estPositionValide(nouveauX, nouveauY)) {
+            // Gestion des effets des tuiles spécifiques
+            int tileId = getEnv().getTileId(nouveauX, nouveauY);
+            if (tileId == 10) { // Lave
+                decrementerPv(5);
+                System.out.println("Link est sur la Lave ! Point de vies restants: " + getPointsDeVie());
+            }
 
-            setX(newX);
-            setY(newY);
-            imageView.setViewport(new Rectangle2D(x, y, 120, 160));
+            if (tileId ==0){
+                imageView.setViewport(new Rectangle2D(position_image_eau, position_image_y, 120, 160));
+            }
+            else {
+                imageView.setViewport(new Rectangle2D(position_image_x, position_image_y, 120, 160));
+            }
+            panneauDeJeu.getChildren().clear();
             imageView.setTranslateX(getX());
             imageView.setTranslateY(getY());
-
+            panneauDeJeu.getChildren().add(imageView);
+            setX(nouveauX);
+            setY(nouveauY);
 
             System.out.println("s'est déplacé en (" + getX() + ", " + getY() + ")");
         } else {
-            System.out.println("Mouvement bloqué par un élément de l'environnement.");
+            System.out.println("Action bloqué par un élément de l'environnement.");
         }
-
-//        System.out.println("Essaye de passer de " + getX() + ", " + getY() + " à " + newX + ", " + newY);
-//
-//        boolean positionValide = getEnv().estPositionValide(newX, newY);
-//
-//        // Gestion des effets des tuiles spécifiques
-//        int tileId = getEnv().getTileId(newX, newY);
-//        if (tileId == 10) { // Lave
-//            decrementerPv(5);
-//            System.out.println("Link perd 5 PV sur la lave. PV restants: " + getPointsDeVie());
-//        }
-//
-//        if (positionValide) {
-//            panneauDeJeu.getChildren().clear();
-//            setX(newX);
-//            setY(newY);
-//            imageView.setTranslateX(getX());
-//            imageView.setTranslateY(getY());
-//            panneauDeJeu.getChildren().add(imageView);
-//
-//            System.out.println("s'est déplacé en (" + getX() + ", " + getY() + ")");
-//        } else {
-//            System.out.println("Mouvement bloqué par un élément de l'environnement.");
-//        }
-//    }
-
-
-
-}
+    }
 }
