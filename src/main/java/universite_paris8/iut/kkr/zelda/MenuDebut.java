4a991f217d00d0bfafe031ba7d3e7de7e97771a9
jkaku <kakujonathan128@gmail>
2024-05-21 15:03:29 +0200

    menu peaufiné

--- src/main/java/universite_paris8/iut/kkr/zelda/MenuDebut.java
+++ src/main/java/universite_paris8/iut/kkr/zelda/MenuDebut.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class MenuDebut extends Application {
@@ -11,8 +12,12 @@ public class MenuDebut extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("GameMenu.fxml"));
+        Scene scene = new Scene(root, 800, 800);
+        String css = this.getClass().getResource("dark.css").toExternalForm();
+        scene.getStylesheets().add(css);
+        Font.loadFont(getClass().getResourceAsStream("/Police/Triforce.ttf"), 48);
         primaryStage.setTitle("Menu Principal");
-        primaryStage.setScene(new Scene(root, 800, 800));
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
