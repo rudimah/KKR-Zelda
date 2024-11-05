d81f92b98b87e1f73efda432edde6bf0aba7e726
hrahman <hamidur.rahman@outlook.fr>
2024-11-05 12:00:41 +0100

    coorection bug reutilisation d'arme

--- src/main/java/universite_paris8/iut/kkr/zelda/modele/Link.java
+++ src/main/java/universite_paris8/iut/kkr/zelda/modele/Link.java
@@ -86,7 +86,11 @@ public class Link extends ActeurEnMouvement{
     public void utiliser(ObjetEnvironnement a){
         objetActuel = a;
         System.out.println(a.getNom() + " est utilisé");
-        a.utiliser();
+        if (armeActuel!= null && a.isReutilisable()){
+            System.out.println("dans le if");
+            getInventaire().getInventaire().add(armeActuel);
+        }
         getInventaire().getInventaire().remove(a);
+        a.utiliser();
     }
 }
