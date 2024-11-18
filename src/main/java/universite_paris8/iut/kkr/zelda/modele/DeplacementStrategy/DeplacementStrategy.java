eacbeddb9f4b7eb72abf9e621c9f32927e3a12ef
Hamidur RAHMAN <123751122+rudimah@users.noreply.github.com>
2024-11-19 00:37:32 +0100

    Delete src/main/java/universite_paris8/iut/kkr/zelda/modele/DeplacementStrategy directory

--- src/main/java/universite_paris8/iut/kkr/zelda/modele/DeplacementStrategy/DeplacementStrategy.java
+++
@@ -1,8 +0,0 @@
-package universite_paris8.iut.kkr.zelda.modele.DeplacementStrategy;
-
-    public interface DeplacementStrategy {
-        public void deplacementDeBase();
-        public boolean verificationObstacles(int x, int y);
-
-    }
-
