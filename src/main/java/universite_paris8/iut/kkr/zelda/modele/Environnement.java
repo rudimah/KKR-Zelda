d6632008c8c1fd7b4ddfafe0f501c8e93178e192
jkaku <kakujonathan128@gmail>
2024-11-19 14:13:01 +0100

    derniere modification dans l'Environnement

--- src/main/java/universite_paris8/iut/kkr/zelda/modele/Environnement.java
+++ src/main/java/universite_paris8/iut/kkr/zelda/modele/Environnement.java
@@ -82,7 +82,7 @@ public class Environnement {
 				if (acteur instanceof Ennemis) {
 					Ennemis ennemi = (Ennemis) acteur;
 					ennemi.decrementerToursFige();
-					if (!ennemi.estFige()) { //	TODO: mettre cette vérification dans les méthode qui sont appler ci dessus (attaquer/ se deplcaer)
+					if (!ennemi.estFige()) {
 						if (ennemi.procheDe(link.getX(), link.getY(), 5)) {
 							ennemi.attaquer(link);
 						} else if (ennemi.getDep().verificationObstacles(ennemi.getX(), ennemi.getY())) {
